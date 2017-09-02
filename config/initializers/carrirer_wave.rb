c8c1bcc1d31c9d1315b8a3b4c7065315470f65ad
sola <solaocodes@gmail.com>
2017-09-02 12:54:14 -0400

    Secure AWS credentials

--- config/initializers/carrirer_wave.rb
+++ config/initializers/carrirer_wave.rb
@@ -3,9 +3,9 @@ if Rails.env.production?
     config.fog_credentials = {
       # Configuration for Amazon S3
       :provider              => 'AWS',
-      :aws_access_key_id     => ENV['AKIAJPJLOQPUKGUAADYQ'],
-      :aws_secret_access_key => ENV['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']
+      :aws_access_key_id     => ENV['S3_ACCESS_KEY'],
+      :aws_secret_access_key => ENV['S3_SECRET_KEY']
     }
-    config.fog_directory     = ENV['solafauxtwitter']
+    config.fog_directory     = ENV['S3_BUCKET']
   end
 end
